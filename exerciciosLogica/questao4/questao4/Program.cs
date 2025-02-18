84bfa5ddab43d4b880c7ddf47fb222a8cab92f0b
victorllcs <vitinholucasangelo2016@gmail.com>
2025-02-18 19:16:23 -0300

    Exercícios de lógica da materia de programação orientada a objetos

---
+++ exerciciosLogica/questao4/questao4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+/*
+Crie um programa que solicite ao usu ́ario um n ́umero inteiro n ̃ao negativo e calcule
+o fatorial desse n ́umero utilizando um loop do-while. O c ́alculo do fatorial deve
+ser implementado em uma fun ̧c ̃ao separada que recebe o n ́umero e retorna o fatorial
+calculado. O programa deve exibir o resultado
+*/
+namespace questao4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um número positivo para que o fatorial seja calculado");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"O fatorial do número {number} é: " + calculaFatorial(number));
+        }
+        public static int calculaFatorial(int numero)
+        {
+            if(numero ==0 || numero ==1)
+                return 1;
+
+            int resultado =1;
+            do 
+            {   
+                resultado *= numero;
+                numero--;
+            }while(numero > 1);
+
+        return resultado;
+        }
+    }
+}
---
+++ exerciciosLogica/questao6/questao6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace questao6
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
